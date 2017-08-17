ae098d4ff52203772d140b28cf944689d5398730
Dingo <oferdottan@gmail.com>
2017-08-18 01:43:53 +0300

    Alpha 17 (initial release)

---
+++ Source/Controller.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using Harmony;
+using Verse;
+
+namespace No_Forced_Slowdown
+{
+	public class Controller : Mod
+	{
+		public Controller(ModContentPack content) : base(content)
+		{
+			HarmonyInstance harmony = HarmonyInstance.Create("dingo.rimworld.no_forced_slowdown");
+
+			harmony.PatchAll(Assembly.GetExecutingAssembly());
+		}
+	}
+}
