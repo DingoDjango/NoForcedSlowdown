0c378bfcce5ecea316ea1c5cde89fdfabc1be295
Dingo <oferdottan@gmail.com>
2018-08-25 19:19:12 +0300

    Compiling for new RW release

--- Source/Main.cs
+++ Source/Main.cs
@@ -9,7 +9,7 @@ namespace NoForcedSlowdown
 	{
 		public Main(ModContentPack content) : base(content)
 		{
-			GetSettings<Settings>();
+			this.GetSettings<Settings>();
 
 #if DEBUG
 			HarmonyInstance.DEBUG = true;
