e673469bbe292801ce6329202e81ec7d23e9c6ed
Dingo <oferdottan@gmail.com>
2024-04-19 18:39:08 +0300

    1.5 release

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("No Forced Slowdown")]
-[assembly: AssemblyCopyright("Copyright © Dingo 2018-2023")]
+[assembly: AssemblyCopyright("Copyright © Dingo 2018-2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.8509.1")]
-[assembly: AssemblyFileVersion("1.4.8509.1")]
+[assembly: AssemblyVersion("1.5.8874.1")]
+[assembly: AssemblyFileVersion("1.5.8874.1")]
